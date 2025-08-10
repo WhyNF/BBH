f57bc179d8372936730505d8a20bda95710f0cfa
WhyNF <wahyulupapw@gmail.com>
2025-08-10 15:08:28 +0800

    WHYNF XBAN
    Read file README to running

---
+++ BBH.py
@@ -0,0 +1,50 @@
+import smtplib
+from email.mime.text import MIMEText
+import time
+import os
+
+os.system("bash whynfx.sh")
+print ("WELCOME ANONYMOUS KILL YOUR TARGET !")
+EMAIL = "budiyusf88@gmail.com"
+PASSWORD = "murah123"
+TO_EMAIL = "support@support.whatsapp.com"
+
+# Minta nomor WA dari terminal
+nomor = input("This is Banned Black Hat !(example: 62xxxxx): ")
+
+SUBJECT = "Laporan WhatsApp"
+BODY = f"مرحبًا بمطوري WhatsApp👋👋👋 أريد 
+أوصي بمواقع المقامرة عبر الإنترنت 👻👻
+الشيء المؤكد هو أنه جاكور 🎰🎰🤑🤑 شباب كتير 
+تابع موقع المقامرة عبر الإنترنت الخاص بنا 😈😈 إذا كنت تريد متابعة موقع المقامرة عبر الإنترنت على WhatsApp 
+يريد مارك زوكربيرج أو موظفو WhatsApp 
+لمتابعة موقعنا يرجى زيارة الموقع 💋💋 أدناه 
+
+https://bento.me/lendirxhut4d
+https://bento.me/lendirxhut4d
+https://bento.me/lendirxhut4d
+
+أوه نعم 👍👍 لدينا 80.000 مشارك في المقامرة عبر الإنترنت و80% منهم أصبحوا مليارديرات🤑🤑 بفضل المقامرة عبر الإنترنت لدينا. إذا كنت عضوًا في WhatsApp أو مطور WhatsApp💻💻 وتريد الانضمام إلى هذا الموقع، فيرجى الاتصال بـ [الرئيس التنفيذي] لدينا.
+تأكد من حصولك على 200,000,000.00 مليون 🤑🤑👻
+يرجى الاتصال بالرقم أدناه
+
+https://api.whatsapp.com/send?phone={nomor}"
+
+JUMLAH_KIRIM = 15
+DELAY = 2
+
+server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+server.login(EMAIL, PASSWORD)
+
+for i in range(JUMLAH_KIRIM):
+    msg = MIMEText(BODY)
+    msg['Subject'] = SUBJECT
+    msg['From'] = EMAIL
+    msg['To'] = TO_EMAIL
+    
+    server.send_message(msg)
+    print(f"[{i+1}] Laporan terkirim untuk nomor {nomor}")
+    time.sleep(DELAY)
+
+server.quit()
+print("banned successfully by BBH, estimate....")
---
+++ README.txt
@@ -0,0 +1,5 @@
+pkg install git python
+git clone https://github.com/USERNAME/BBH.git
+cd BBH
+chmod +x efek_whynfx.sh
+python BBH.py
